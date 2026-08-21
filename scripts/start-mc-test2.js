ef17be41adfbf3203ade25ffadc6c50ce7613839
rkendel1 <votersvoices1@gmail.com>
2026-08-21 14:06:04 -0400

    making ready

--- scripts/start-mc-test2.js
+++
@@ -1,4 +0,0 @@
-// Wrapper to start mc-test2 Next.js server from any cwd
-process.chdir("C:/Users/justs/Documents/mc-test2/mission-control");
-process.argv = [process.argv[0], "dev", "-p", "3100"];
-require("C:/Users/justs/Documents/mc-test2/mission-control/node_modules/next/dist/bin/next");
--- src-tauri/src/agent.rs
+++
@@ -1,9 +0,0 @@
-//! Agent runtime management.
-
-pub struct AgentManager;
-
-impl AgentManager {
-    pub fn new() -> Self {
-        Self
-    }
-}
